a57c9393bbfb884881571824aeb4e68010ac4f66
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2018-07-24 07:44:34 +0900

    complete js-converter

--- convert.js
+++ convert.js
@@ -1,6 +1,14 @@
-function getTextArea(){
-    var text = document.getElementById('inputArea').value;
-    console.log(text);
-    return text;
-}
-
+var converter = new Vue({
+    el:'#converter',
+    data:{
+        inputtext:"",
+        outputtext:""
+    },
+    methods:{
+        convert: function () {
+            var str = this.inputtext.replace(/\r?\n/g, '\\n');
+            str = str.replace(/\r?"/g, '\\"');
+            this.outputtext = str;
+        }
+    }
+});
